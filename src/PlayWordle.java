e9c485801d6dcee264715700575dd7ad6edd978b
johnolszewskim <johnolszewskim@gmail.com>
2023-08-06 13:09:23 -0700

    updated to newest version

--- src/PlayWordle.java
+++ src/PlayWordle.java
@@ -3,114 +3,116 @@ import java.io.*;
 import wordlejava.*;
 
 /**
- * 
+ *
  * Console-based game based that allows a user to play a single game of Wordle. Should
  * be run in an ANSI console to get color-coded results. Game finishes and prints the resuls
  * and then prints the WordleSolver results for the user to compare.
- * 
+ *
  * @author johnmatthew
  *
  */
 public class PlayWordle {
 
-	public static final String ANSI_RESET = "\033[0m";
-	public static final String ANSI_GREEN = "\u001B[32m";
-	public static final String ANSI_YELLOW = "\u001B[33m";
-	
-	
-	
+	private static final String ANSI_RESET = "\033[0m";
+	private static final String ANSI_GREEN = "\u001B[32m";
+	private static final String ANSI_YELLOW = "\u001B[33m";
+	private static final String ANSI_RED = "\033[0;31m";
+
+	private static Hashtable<String, String> resultsKeyboard;
+
 	public static void main(String []args) {
-		
+
 		System.out.print(ANSI_RESET);
-		
+		resultsKeyboard = initResultsKeyboard();
+
 		LinkedList<String> library = GuessManager.importWordsToLibraryFromWeb(5);
 		LinkedList<String> libraryCopy = new LinkedList<>();
 		libraryCopy.addAll(library);
-		
+
 		WordleGame randomStandardGame = WordleGame.getRandomStandardGame(library);
-		WordleRecord result = play(randomStandardGame);
+		WordleRecord result = play(randomStandardGame, libraryCopy);
 		String wordle = randomStandardGame.wordle();
-		
+
 		System.out.println(result);
 		WordleSolver ws = new WordleSolver(new WordleGame(wordle, WordleGame.STANDARD_GUESSES), libraryCopy);
 		System.out.println("WordleSolver:\n" + ws.solve());
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Plays the WordleGame and returns the WordleResult
-	 * 
+	 *
 	 * @param game the WorldGame instance to be played
 	 * @return result when the game is over
 	 */
-	private static WordleRecord play(WordleGame game) {
-		
+	private static WordleRecord play(WordleGame game, LinkedList<String> library) {
+
 		LinkedList<String> guesses = new LinkedList<>();
-		
-		
-		
+
 		while (!game.gameIsOver()) {
-			
+
 			printResultsGrid(game);
-			String nextGuess = getNextGuess(game);
+			printKeyboard();
+			String nextGuess = getNextGuess(game, library);
 			game.guess(nextGuess);
 			guesses.add(nextGuess);
-			System.out.print("\033[H\033[2J");  
-			System.out.flush(); 
+			System.out.print("\033[H\033[2J");
+			System.out.flush();
 		}
-		
+
 		printResultsGrid(game);
-		
+
 		return new WordleRecord(game.wordle(), guesses, game.getWin());
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Gets the next guess from the use. Checks the validity of the word based on the game.
-	 * 
+	 *
 	 * @param game the game for which make the guess.
 	 * @return the guess that the user inputs
 	 */
-	private static String getNextGuess(WordleGame game) {
-		
+	private static String getNextGuess(WordleGame game, LinkedList<String> library) {
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Guess word: ");
 		String guess = scan.nextLine().toLowerCase();
 		System.out.println();
-		while(!WordleGame.isValidWordleGuess(guess, game.getWordleLength())) {
+		while(!WordleGame.isValidWordleGuess(guess, game.getWordleLength(), library)) {
+
 			System.out.print("Guess word: ");
 			guess = scan.nextLine().toLowerCase();
 			System.out.println();
 		}
-		
+
 		return guess;
 	}
 
 	/**
-	 * 
+	 *
 	 * Prints the grid based on the results of the guess for user information.
-	 * Green text means the letter is correct. Yellow text means the letter is 
-	 * in the word but in the wrong index. Standard text means the letter is 
+	 * Green text means the letter is correct. Yellow text means the letter is
+	 * in the word but in the wrong index. Standard text means the letter is
 	 * not in the wordle.
-	 * 
+	 *
 	 * @param game the WorldGame whose grid is to be printed
 	 */
 	private static void printResultsGrid(WordleGame game) {
 		System.out.println("    WORDLE");
 
 		for(int i = 0; i < game.getNumGuesses(); i++) {
-			
+
 			char[] guess = game.getGuess(i);
-			
+
 			if(game.nextGuessIndex() == i) {
 				System.out.print("> ");
 			} else {
 				System.out.print("  ");
 			}
-			
+
 			for(int j = 0; j < game.getWordleLength(); j++) {
-				
+
 				if(guess[j] == Character.MIN_VALUE) {
 					System.out.print("_ ");
 				} else {
@@ -122,11 +124,42 @@ public class PlayWordle {
 		System.out.println(); // formatting
 	}
 
+	private static void printKeyboard() {
+
+		System.out.print(resultsKeyboard.get("q") + "q " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("w") + "w " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("e") + "e " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("r") + "r " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("t") + "t " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("y") + "y " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("u") + "u " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("i") + "i " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("o") + "o " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("p") + "p " + ANSI_RESET + "\n ");
+		System.out.print(resultsKeyboard.get("a") + "a " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("s") + "s " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("d") + "d " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("f") + "f " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("g") + "g " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("h") + "h " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("j") + "j " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("k") + "k " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("l") + "l " + ANSI_RESET + "\n  ");
+		System.out.print(resultsKeyboard.get("z") + "z " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("x") + "x " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("c") + "c " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("v") + "v " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("b") + "b " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("n") + "n " + ANSI_RESET);
+		System.out.print(resultsKeyboard.get("m") + "m " + ANSI_RESET);
+		System.out.println();
+	}
+
 	/**
-	 * 
-	 * Prints a letter in the grid with the correct color based on the related 
+	 *
+	 * Prints a letter in the grid with the correct color based on the related
 	 * result.
-	 * 
+	 *
 	 * @param result determines the color to print the letter
 	 * @param letter the letter to print
 	 */
@@ -135,15 +168,54 @@ public class PlayWordle {
 		switch(result) {
 			case 1:
 				System.out.print(ANSI_GREEN + Character.toString(letter) + ANSI_RESET + " ");
+				resultsKeyboard.replace(Character.toString(letter), ANSI_GREEN);
 				break;
 			case 2:
 				System.out.print(ANSI_YELLOW + Character.toString(letter) + ANSI_RESET + " ");
+				resultsKeyboard.replace(Character.toString(letter), ANSI_YELLOW);
+				break;
+			case 0:
+				System.out.print(ANSI_RED + Character.toString(letter) + ANSI_RESET + " ");
+				resultsKeyboard.replace(Character.toString(letter), ANSI_RED);
 				break;
 			default:
 				System.out.print(ANSI_RESET + Character.toString(letter) + " ");
+				resultsKeyboard.replace(Character.toString(letter), ANSI_RESET);
 		}
-		
+
 		System.out.print(ANSI_RESET);
 	}
 
+	private static Hashtable<String,String> initResultsKeyboard() {
+
+		Hashtable<String,String> keyboard = new Hashtable<>();
+		keyboard.put("a", ANSI_RESET);
+		keyboard.put("b", ANSI_RESET);
+		keyboard.put("c", ANSI_RESET);
+		keyboard.put("d", ANSI_RESET);
+		keyboard.put("e", ANSI_RESET);
+		keyboard.put("f", ANSI_RESET);
+		keyboard.put("g", ANSI_RESET);
+		keyboard.put("h", ANSI_RESET);
+		keyboard.put("i", ANSI_RESET);
+		keyboard.put("j", ANSI_RESET);
+		keyboard.put("k", ANSI_RESET);
+		keyboard.put("l", ANSI_RESET);
+		keyboard.put("m", ANSI_RESET);
+		keyboard.put("n", ANSI_RESET);
+		keyboard.put("o", ANSI_RESET);
+		keyboard.put("p", ANSI_RESET);
+		keyboard.put("q", ANSI_RESET);
+		keyboard.put("r", ANSI_RESET);
+		keyboard.put("s", ANSI_RESET);
+		keyboard.put("t", ANSI_RESET);
+		keyboard.put("u", ANSI_RESET);
+		keyboard.put("v", ANSI_RESET);
+		keyboard.put("w", ANSI_RESET);
+		keyboard.put("x", ANSI_RESET);
+		keyboard.put("y", ANSI_RESET);
+		keyboard.put("z", ANSI_RESET);
+
+		return keyboard;
+	}
 }
--- src/wordlejava/WordleGame.java
+++ src/wordlejava/WordleGame.java
@@ -3,22 +3,22 @@ package wordlejava;
 import java.util.*;
 
 /**
- * 
+ *
  * @author johnmatthew
  *
  * Representation of New York Times word game Wordle.
  * Class intended to be used in conjunction with annother
  * classes the holds an instance of this class. Does not 
  * include user interaction.
- * 
+ *
  * The game is intended to be flexible based on the Wordle and 
  * the number of guess that are specified during instance creation. 
  *
  */
 public class WordleGame {
-	
-			
-	
+
+
+
 	public static int STANDARD_GUESSES = 6;
 	public static int STANDARD_LENGTH = 5;
 	private char[][] grid;
@@ -26,104 +26,104 @@ public class WordleGame {
 	private int nextGuessIndex = 0;
 	private int lastGuessIndex = -1;
 	private boolean gameIsOver = false;
-	
+
 	/**
-	 * 
+	 *
 	 * Only constructor for WordleGame. Requires minimum amount of
 	 * parameters required to play the game.
-	 * 
+	 *
 	 * @param wordle single word that will be unknown to the person playing
 	 * @param numGuesses number of guesses the user will be allowed
 	 */
 	public WordleGame(String wordle, int numGuesses) {
-		
+
 		this.wordle = wordle.toLowerCase();
 		initializeGrid(wordle.length(), numGuesses);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * initializes the char[] that holds the user guesses
-	 * 
+	 *
 	 * @param wordleLength the number of letters in the World
 	 * @param numGuesses the number of guesses the player will be allowed
 	 */
 	private void initializeGrid(int wordleLength, int numGuesses) {
-		
+
 		this.grid = new char[numGuesses][wordleLength];
-		
+
 		for(int i = 0; i < numGuesses; i++) {
-			
+
 			char[] guess = new char[wordleLength];
 			grid[i] = guess;
 		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * A single guess for the wordle. Boundary cases and invalid words
 	 * are checked for then the word is placed in the grid.
-	 * 
+	 *
 	 * @param word the word the the player guesses
 	 * @return true if the word was successfully placed in the grid.
 	 * 			false if the word wasn't placed.
 	 */
 	public boolean guess(String word) {
-		
+
 		if(word == null) { return false; }
 		if(word.length() != this.wordle.length()) { return false; }
-		if(this.gameIsOver) { 
-			return false; 
+		if(this.gameIsOver) {
+			return false;
 		}
 		// TODO: if word isnt a valid word
-		
+
 		this.placeWord(word);
 		this.nextGuessIndex++;
 		this.lastGuessIndex++;
-		
+
 		if(this.nextGuessIndex == this.grid.length) {
 			this.nextGuessIndex--;
 			this.gameIsOver = true;
 		}
-		
+
 		if(String.valueOf(this.grid[this.lastGuessIndex]).equals(this.wordle)) {
-			
+
 			this.gameIsOver = true;
 		}
 
 		return true;
 	}
-	
+
 	/**
 	 * Places the word in the correct index in the grid.
 	 * Checks for boundary cases and invalid words should be done
 	 * prior to calling the method.
-	 * 
+	 *
 	 * @param word
 	 */
 	private void placeWord(String word) {
-		
+
 		for(int i = 0; i < word.length(); i++) {
-			
+
 			this.grid[this.nextGuessIndex][i] = word.charAt(i);
 		}
 	}
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * Prints the grid of sequential guesses top to bottom. Blanks
 	 * represent unused guesses.
-	 * 
+	 *
 	 * For debugging, relevant instance variable values are printed.
 	 */
 	public void printGrid() {
-		
+
 //		System.out.println("Wordle: " + this.wordle); // for testing
-		
+
 		for(int i = 0; i < this.grid.length; i++) {
 			for(int j = 0; j < this.grid[i].length; j++) {
-				
+
 				if(this.grid[i][j] == Character.MIN_VALUE) {
 					System.out.print("_ ");
 				} else {
@@ -137,160 +137,169 @@ public class WordleGame {
 //		System.out.println("GAME IS OVER: " + this.gameIsOver); // for testing
 //		System.out.println(); // for testing
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Determines if the last guess won the game.
-	 * 
+	 *
 	 * @return true if the last guess was the Wordle.
 	 */
 	public boolean getWin() {
-		
+
 		return String.valueOf(this.grid[this.lastGuessIndex]).equals(this.wordle);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Determines the appropriate response for a single letter. Uses grid information
 	 * to compare to the Wordle. Any letter in the grid can be checked, not 
 	 * just the last guess.
-	 * 
+	 *
 	 * @param guess the guess index beginning with 0.
 	 * @param letter the letter index beginning with 0.
 	 * @return 1 if the letter is correct. 2 If the letter is in the wordle but
 	 * 			is in the incorrect index. 0 if the letter is not in the wordle.
 	 */
 	public int getResult(int guess, int letter) {
-		
+
 		if(this.grid[guess][letter] == this.wordle.charAt(letter)) {
 			return 1;
 		}
-		
+
 		if(this.wordle.indexOf(this.grid[guess][letter]) != -1) {
 			return 2;
 		}
-					
+
 		return 0;
 	}
-	
+
 	public char[] getGuess(int guess) {
-		
+
 		return this.grid[guess];
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Gets the results of the last guess taken by the player. If the 
 	 * game is over, gets the final guess.
-	 * 
+	 *
 	 * @return Array representing the results of the last guess. 
 	 */
 	public int[] getLastResults() {
-		
+
 		int[] results = new int[wordle.length()];
-		
+
 		for(int i = 0; i < this.wordle.length(); i++) {
 			results[i] = this.getResult(this.lastGuessIndex, i);
 		}
-		 
-		
+
+
 		return results;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Used to get the last guess placed in the grid. 
-	 * 
+	 *
 	 * @return char[] representation of the last guess.
 	 */
 	public char[] getLastGuess() {
-		
+
 		return this.grid[this.lastGuessIndex];
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * The number of letters in the wordle.
-	 * 
+	 *
 	 * @return int representing the number of letters in the wordle.
 	 */
 	public int getWordleLength() { return this.wordle.length(); }
-	
+
 	/**
-	 * 
+	 *
 	 * Used to check if the game is over.
-	 * 
+	 *
 	 * @return true if wordle has been guessed or the game is over
 	 */
 	public boolean gameIsOver() { return this.gameIsOver; }
 
-	public int lastGuessIndex() { 
-		
+	public int lastGuessIndex() {
+
 		if(this.gameIsOver && !this.getWin()) {
 			return 0;
 		}
-		
+
 		return this.lastGuessIndex;
-		
+
 	}
-	
+
 	/**
 	 * Returns the index of the next guess.
-	 * 
+	 *
 	 * @return index of the next guess. The last index if the last guess has
 	 * been made
 	 */
 	public int nextGuessIndex() { return this.nextGuessIndex; }
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * Checks for the validity of a word based on a game.
-	 * 
+	 *
 	 * @param guess word to be checked
 	 * @param wordleLength the length of the wordle to be compared
 	 * @return false if the word is not the same length, null or contains 
 	 * 			anything but alphabetical characters
 	 */
 	public static boolean isValidWordleGuess(String guess, int wordleLength) {
-		
+
 		if(guess == null) { return false; }
-		
+
 		if(guess.length() != wordleLength) { return false; }
-		
+
 		if(!guess.matches("^[a-zA-Z]*$")) { return false; }
-		
+
 		return true;
 	}
-	
+
+	public static boolean isValidWordleGuess(String guess, int wordleLength, LinkedList<String> library) {
+
+		if(!library.contains(guess)) {
+			return false;
+		}
+
+		return isValidWordleGuess(guess, wordleLength);
+	}
+
 	/**
-	 * 
+	 *
 	 * Returns a game with the standard number of guess based on the standard length
 	 * picked randomly from a library. Restricted to the first 2309 indicies of a 
 	 * library sorted by frequency to align with the NYT game.
-	 * 
+	 *
 	 * @param library word list from which to pick the wordle
 	 * @return new standard game instance
 	 */
 	public static WordleGame getRandomStandardGame(List<String> library) {
-		
+
 		String wordle = library.get(new Random().nextInt(2309));
-		
+
 		return new WordleGame(wordle, STANDARD_GUESSES);
 	}
 
 	/**
-	 * 
+	 *
 	 * Returns the number of guesses in the Wordle game
-	 * 
+	 *
 	 * @return int of the number of guesses
 	 */
 	public int getNumGuesses() { return this.grid.length; }
-	
+
 	/**
-	 * 
+	 *
 	 * Gets the wordle
-	 * 
+	 *
 	 * @return String of the wordle
 	 */
 	public String wordle() { return this.wordle; }
